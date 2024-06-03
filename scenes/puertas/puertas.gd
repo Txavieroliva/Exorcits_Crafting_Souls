a17fd4e05ed73b9c6ea36d5bd7c1bb830329610e
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-06-03 19:54:34 -0300

    Modificaciones varias

--- scenes/puertas/puertas.gd
+++ scenes/puertas/puertas.gd
@@ -1,11 +1,10 @@
 extends StaticBody2D
 
-
 var entered = false
 
 func _on_area_2d_body_entered(body: CharacterBody2D):
 	entered = true
-
+	Global.from_level = get_parent().name
 
 func _on_area_2d_body_exited(body):
 	entered = false
@@ -13,4 +12,4 @@ func _on_area_2d_body_exited(body):
 func _physics_process(delta):
 	if entered == true:
 		if Input.is_action_just_pressed("ui_accept"):
-			get_tree().change_scene_to_file("res://scenes/scenery/casa_1/habitaciones/habitacion_2.tscn")
+			get_tree().change_scene_to_file("res://scenes/scenery/casa_1/habitaciones/" + self.name + ".tscn")
