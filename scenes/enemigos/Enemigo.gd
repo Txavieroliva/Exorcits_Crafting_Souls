72093e1e0512603a4f469fa12905d36f817ef473
Thiago Xavier <xavieroliva.thiagoe@gmail.com>
2024-06-13 20:46:04 -0300

    Se elmina codigo innecesario

--- scenes/enemigos/Enemigo.gd
+++ scenes/enemigos/Enemigo.gd
@@ -9,16 +9,6 @@ func _ready():
 func _physics_process(delta):
 	follow_player(delta)
 
-
-#func _on_area_2d_body_entered(body):
-	#player = JugadorGlobal.obtener_ref_jugador()
-	#body = player
-#
-#
-#func _on_area_2d_body_exited(body):
-	#player = null
-	#body = player
-
 func follow_player(delta):
 	if player != null:
 		velocity = global_position.direction_to(player.global_position) * SPEED
